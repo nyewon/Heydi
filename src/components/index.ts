2c1a530fec5f6e0730f2fb189fb993dec2de150f
nohyewon <yw4913@naver.com>
2026-01-12 00:07:23 +0900

    :wrench:[chore]: community component 폴더 생성 및 이동

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,14 +1,10 @@
 export * from "./common/index";
 export * from "./modal/index";
-export * from "./report/index";
 export * from "./dropdown/index";
+
+export * from "./community/index";
+export * from "./report/index";
 
 // Diary
 export { default as DiaryCard } from "./DiaryCard";
 export { default as DiaryInfoBox } from "./DiaryInfoBox";
-
-// Community
-export { default as CommunityCard } from "./CommunityCard";
-export { default as SelectDiaryCard } from "./SelectDiaryCard";
-export { default as CommentItem } from "./CommentItem";
-export { default as Comment } from "./Comment";
