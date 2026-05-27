b687b1d7a7e662079e10172637841ebf55686d6b
nohyewon <yw4913@naver.com>
2026-05-28 01:27:41 +0900

    :recycle:[refactor]: 게시글 생성 일기 선택 수정

--- src/services/community.ts
+++ src/services/community.ts
@@ -14,10 +14,11 @@ export const updatePost = async (
 };
 
 // 커뮤니티 게시글 작성용 일기 선택
-export const selectDiaryForPost = async (diary_id: number) => {
+export const selectDiaryForPost = async (diaryId: number) => {
   const res = await instance.post("/community/selectdiary", {
-    diary_id,
+    diaryId,
   });
+
   return res.data;
 };
 
